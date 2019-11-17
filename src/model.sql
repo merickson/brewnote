9b96b0a9012ac6f5009993fea4dcba3f8bb81ad5
Matt Erickson <matt@spideroak.com>
2019-11-16 23:09:30 -0600

    Made fermentable types a table and using numeric type.

--- src/model.sql
+++ src/model.sql
@@ -9,21 +9,22 @@ CREATE TABLE IF NOT EXISTS brands (
     name varchar(100) NOT NULL CHECK (name <> '')
 );
 
-CREATE TYPE fermentable_type AS ENUM (
-    'applejuice',
-    'honey'
-);
-
 CREATE TYPE measure_type AS ENUM (
     'dry_weight_g',
     'dry_volume_l',
     'liquid_volume_l'
 );
 
+CREATE TABLE IF NOT EXISTS fermentable_types (
+    id integer PRIMARY KEY,
+    name varchar(100),
+    measure measure_type
+);
+
 CREATE TABLE IF NOT EXISTS fermentable (
     id integer PRIMARY KEY,
     brand integer REFERENCES brands.id,
-    type fermentable_type,
+    type integer REFERENCES fermentable_types.id,
     size integer,
     measure measure_type,
     name varchar(100),
@@ -63,37 +64,44 @@ CREATE TABLE IF NOT EXISTS additive_types (
 )
 
 CREATE TABLE IF NOT EXISTS additives (
-    id integer PRIMARY_KEY,
+    id integer PRIMARY KEY,
     type integer REFERENCES additive_types.name,
     brand integer REFERENCES brands.id,
     measure measure_type NOT NULL,
     name varchar(100),
-)
+);
+
+CREATE TABLE IF NOT EXISTS equipment_types (
+    id integer PRIMARY KEY,
+    name varchar(100),
+    measure measure_type,
+    size numeric,
+);
 
 CREATE TABLE IF NOT EXISTS fermentation (
     id integer PRIMARY KEY,
     batch integer REFERENCES batch.id,
     startdate timestamp DEFAULT CURRENT_TIMESTAMP,
     enddate timestamp,
-    start_specific_gravity real,
-    end_specific_gravity real,
-    start_ph real,
-    end_ph real,
-    start_ta real,
-    end_ta real
+    start_specific_gravity numeric,
+    end_specific_gravity numeric,
+    start_ph numeric,
+    end_ph numeric,
+    start_ta numeric,
+    end_ta numeric
 );
 
 CREATE TABLE IF NOT EXISTS fermentation_fermentables (
     fermentation integer REFERENCES fermentation.id,
     fermentable integer REFERENCES fermentable.id,
-    amount real,
+    amount numeric,
     CONSTRAINT UNIQUE(fermentation, fermentable) 
 );
 
 CREATE TABLE IF NOT EXISTS fermentation_yeasts (
     fermentation integer REFERENCES fermentation.id,
     yeast integer REFERENCES yeastpacket.id,
-    amount real,
+    amount numeric,
     CONSTRAINT UNIQUE(fermentation, fermentable)
 );
 
@@ -101,5 +109,5 @@ CREATE TABLE IF NOT EXISTS fermentation_additives (
     fermentation integer REFERENCES fermentation.id,
     additive integer REFERENCES additives.id,
     added_time timestamp,
-    amount real
+    amount numeric
 )
